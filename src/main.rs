82f3a79a7bd4bd6dbce9ba21480d10c2fa72a81c
Your Name <you@example.com>
2024-04-25 14:33:23 +1000

    Fixed so that choosing a player doesn't crash the program.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{stdout, stdin, Write};
+use std::io::{stdin, stdout, Write};
 
 fn main() {
     println!("Choose a mode:");
@@ -15,9 +15,11 @@ fn main() {
             println!("Beginning single player mode.");
             print!("Would you like to be Player 1 or Player 2? ");
             stdout().flush().unwrap();
+            x = String::new();
             stdin().read_line(&mut x).unwrap();
+            dbg!(x.trim());
             let player: u8 = x.trim().parse::<u8>().unwrap();
-        },
+        }
         2 => {
             println!("Beginning two player mode.");
             let mut player: u8 = 1;
@@ -33,13 +35,15 @@ fn main() {
                         eprintln!("Invalid spot: '{}'", z);
                     } else if board[z].1 {
                         println!("Player {} has already claimed this spot!", board[z].0);
-                    } else { break };
+                    } else {
+                        break;
+                    };
                 }
                 board[z].1 = true;
                 board[z].0 = player;
                 player = if player == 1 { 2 } else { 1 }
             }
-        },
+        }
         3 => {
             println!("Pick a number between 1 and 9, each corresponding to a slot in the grid.");
         }
@@ -49,15 +53,24 @@ fn main() {
 
 fn print_board(board: [(u8, bool); 9]) {
     for i in 0..3 {
-        println!("| {} | {} | {} |", x(board, i, 0), x(board, i, 1), x(board, i, 2));
+        println!(
+            "| {} | {} | {} |",
+            x(board, i, 0),
+            x(board, i, 1),
+            x(board, i, 2)
+        );
     }
 }
 
 fn x(board: [(u8, bool); 9], i: usize, j: usize) -> String {
-    if board[i * 3 + j].1 { match board[i * 3 + j].0 {
-        1 => "o",
-        2 => "x",
-        a => panic!("Invalid player {}", a)
-    }} else { " " }.to_owned()
+    if board[i * 3 + j].1 {
+        match board[i * 3 + j].0 {
+            1 => "o",
+            2 => "x",
+            a => panic!("Invalid player {}", a),
+        }
+    } else {
+        " "
+    }
+    .to_owned()
 }
-
